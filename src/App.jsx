6a6d0b65d5e6881deab9168a35ecdae8217e572c
katemba fahad <katsfahad008@gmail.com>
2024-09-30 13:39:57 +0300

    Add task component and react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Task from "./components/TaskPage";
+
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Task/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
     );
 }
  
