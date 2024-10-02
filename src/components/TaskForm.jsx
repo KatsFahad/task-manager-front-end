5317cb2b5cb6c2231fe58a5c61f8b9e817077c11
katemba fahad <katsfahad008@gmail.com>
2024-10-02 12:43:34 +0300

    Complete taskform with post api to add data to the database

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,10 +1,54 @@
+// import { json, response } from "express";
+import { useState } from "react";
 
 const TaskForm = () => {
+
+    const [task, setTask] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+       if(task){
+        const apiUrl = 'https://task-manager-back-end-hrfw.onrender.com/tasks'
+
+        const requestObject = {
+            method: 'POST',
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                title: task
+            })
+        }
+        fetch(apiUrl, requestObject)
+        .then((response)=>{
+            if(response.ok){
+                return response.json()
+            }else{
+                throw new Error('failed to add task')
+            }
+        })
+        .then((data)=>{
+            if(data){
+                alert('task added')
+                setTask('')
+            }else{
+                alert('Adding task failed')
+            }
+        })
+
+       }else{
+        alert('Task field cannot be empty')
+       }
+
+    }
+
+
   return (
     <div>
-      <form action="">
-        <input type="text" placeholder="Enter a Task" />
-        <button>Add Task</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={task} placeholder="Enter a Task" onChange={e => setTask(e.target.value) } />
+        <button type="submit">Add Task</button>
       </form>
     </div>
   );
