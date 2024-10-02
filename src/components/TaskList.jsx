2e5a72cd253e01d32e415155a8a21b4d36fd3d30
katemba fahad <katsfahad008@gmail.com>
2024-10-02 17:35:04 +0300

    Setup delete api to delete tasks from the database

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -13,13 +13,41 @@ const TaskList = () => {
             return response.json()
         })
         .then((data)=>{
-            console.log(data)
             setTasks(data)
         })
     }
     useEffect(()=>{
         fetchTasks()
     },[])
+
+    // const deleteTask = (deleteitem) =>{
+    //     const newTasks = tasks.filter((item)=> item !== deleteitem)
+    //     setTasks(newTasks)
+    // }
+    const deleteTask = async (task) => {
+        try {
+          const response = await fetch(
+            `https://task-manager-back-end-hrfw.onrender.com/tasks/${task.id}`,
+            {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+    
+          if (response.ok) {
+            const newTasks = tasks.filter((item) => item.id !== task.id);
+            setTasks(newTasks);
+            alert("Task deleted successfully");
+          } else {
+            alert("Failed to delete task");
+          }
+        } catch (error) {
+          console.error("Error deleting task:", error);
+          alert("Failed to delete task");
+        }
+    }
   return (
     <div>
         <h2>Tasks Below</h2>
@@ -27,7 +55,7 @@ const TaskList = () => {
         {
             tasks !== null ? (
                 tasks.map((item, index)=>{
-                    return <li key={index}>{item.title}</li>
+                    return <li key={index}>{item.title}<span><button onClick={()=> deleteTask(item)}>Delete</button></span></li>
                 })
             ):
             <h3>No Tasks Available</h3>
